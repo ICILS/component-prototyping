01d889f6a141a2d89664793f2de1f6ec2790aaf0
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2022-11-14 19:15:13 +1100

    Use vuetify vite config from theme

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,60 @@
+/**
+ * plugins/vuetify.js
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Blueprints
+import { md3 } from 'vuetify/blueprints'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+// Components
+import { VBtn } from 'vuetify/components'
+
+export default createVuetify({
+  aliases: {
+    VBtnAlt: VBtn,
+  },
+  blueprint: md3,
+  // https://next.vuetifyjs.com/features/global-configuration/
+  defaults: {
+    global: {
+      rounded: 'sm',
+    },
+    VAppBar: {
+      flat: true,
+    },
+    VBtn: {
+      color: 'primary',
+      height: 44,
+      // minWidth: 120,
+    },
+    VBtnAlt: {
+      color: 'primary',
+      height: 48,
+      minWidth: 120,
+      variant: 'outlined',
+    },
+    VSheet: {
+      color: '#212121',
+    },
+  },
+  // https://next.vuetifyjs.com/features/theme/
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        dark: true,
+        colors: {
+          primary: '#1697f6',
+        },
+      },
+    },
+  },
+})
