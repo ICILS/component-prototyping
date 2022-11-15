6fc95cbf4eeffde8c2ed41392632df8ebee4c299
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2022-11-15 21:11:10 +1100

    Button style tweaks

--- src/components/RunResetButton.vue
+++ src/components/RunResetButton.vue
@@ -3,9 +3,29 @@ import { store } from './store'
 </script>
 
 <template>
-  <v-btn min-width="120" flat :color="store.isPlaying ? 'blue' : 'green'" @click="store.toggleButton()">
-    <v-icon size="large" :icon="store.isPlaying ? 'mdi-restart' : 'mdi-play'" />
+  <v-btn
+    variant="flat"
+    class="px-3"
+    min-width="105"
+    flat
+    :color="store.isPlaying ? 'blue' : 'green'"
+    rounded="pill"
+    @click="store.toggleButton()"
+  >
+    <template #prepend>
+      <v-icon
+        transition="slide-y-transition"
+        class="mx-0"
+        size="x-large"
+        :icon="store.isPlaying ? 'mdi-restart' : 'mdi-play'"
+      />
+    </template>
+
     <span v-if="store.isPlaying" class="text-button"> Reset </span>
-    <span v-else class="text-button"> Run </span>
+    <span v-else class="text-button">Run</span>
   </v-btn>
 </template>
+
+<style lang="scss" scoped>
+
+</style>
